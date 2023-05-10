332bd0755487e5d519da121f18147bc168f201e9
dwcs_code_2223 <dwcs.code.2223@gmail.com>
2023-05-10 20:24:44 +0200

    A7.4-enunciado

--- src/Controller/NotaController.php
+++ src/Controller/NotaController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Nota;
 use App\Service\NotaService;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class NotaController extends AbstractController
 {
@@ -31,35 +33,60 @@ class NotaController extends AbstractController
 
     //Opción B)
     #[Route('/nota/new', name: 'app_nota_new')]
-    public function index(NotaService $notaService): Response
+    public function crear(Request $request, NotaService $notaService): Response
     {
         $nota = new Nota();
-        $nota->setTitulo("Mi tercera nota");
-        $nota->setDescripcion("Mi tercera nota con Symfony y Doctrine");
+        $valid = false;
 
-        $nota = $notaService->create($nota);
+        if ($request->getMethod() === 'POST') {
+            $valid = true;
+            //Se usa $request->request para obtener variables enviadas por POST
+            //Se usa $request->query para obtener variables enviadas por GET
+            $titulo = $request->request->get('titulo');
+            $desc =  $request->request->get('desc');
 
+            if (isset($titulo) && empty($titulo)) {
+                $this->addFlash('warning', "El campo título es obligatorio");
+                $valid = false;
+            } else {
+                $nota->setTitulo($titulo);
+            }
 
-        return $this->render('nota/index.html.twig', [
-            'controller_name' => 'NotaController',
-            'nota' => $nota
-        ]);
+            if (isset($desc) && empty($desc)) {
+                $this->addFlash('warning', "El campo descripción es obligatorio");
+                $valid = false;
+            } else {
+                $nota->setDescripcion($desc);
+            }
+        }
+
+        if ($valid) {
+            $nota = $notaService->create($nota);
+            $this->addFlash('success', "Se ha creado la nota correctamente");
+            return $this->redirectToRoute('app_nota_list');
+        } else {
+
+            return $this->render('nota/crear.html.twig', 
+        ['nota' => $nota]);
+        }
     }
 
 
     #[Route('/nota', name: 'app_nota_list')]
     public function list(NotaService $notaService): Response
     {
-        
+
 
         $notas = $notaService->list();
+        $contador = sizeof($notas);
+        if ($contador > 0) {
 
+            $this->addFlash("info", "Se han encontrado $contador notas");
+        }
 
         return $this->render('nota/list.html.twig', [
             'controller_name' => 'NotaController',
             'notas' => $notas
         ]);
     }
-
-
 }
